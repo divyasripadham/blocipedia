0d1e92a408edcc5f77126a46e3c3f51548ef5324
divyasripadham <divya_sri16@yahoo.com>
2015-12-09 10:18:11 -0500

    Blocipedia Checkpoint 8, Create and View private wikis

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,5 +2,5 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
   default_scope { order('updated_at DESC') }
   validates :user, presence: true
-  scope :visible_to, -> (user) { user ? all : where(private: false) }
+  scope :visible_to, -> (user) { (user.admin?) ? all : ((user.premium?) ? (where("user_id = ? OR private = ?",user.id,false)) : where(private: false)) }
 end
