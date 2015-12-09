179e5c72bc98f1e02f50c24449f580399138ff59
divyasripadham <divya_sri16@yahoo.com>
2015-12-09 12:16:29 -0500

    Blocipedia Checkpoint 8, downgrade private to public

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,14 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
-  
+
   def user_downgrade
+    @user = User.find(params[:user_id])
+    @wikis = @user.wikis
     authorize :user
     # current_user.update_attribute(:role, 0)
     if current_user.standard!
+       @wikis.update_all(:private => false)
        flash[:notice] = "You have been downgraded to Standard"
     else
        flash[:error] = "There was an error downgrading the user. Please try again."
